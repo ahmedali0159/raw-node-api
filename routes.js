878ab9725e7eb15afb33995111cde1dfbb9f7130
ahmedali0159 <pirates0159@gmail.com>
2021-07-26 12:16:41 +0600

    create token post

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 // dependencies
 const {sampleHandler} = require('./handlers/handlersRoutes/sampleHandler');
 const {userHandler} = require('./handlers/handlersRoutes/userHandler')
+const {tokenHandler} = require('./handlers/handlersRoutes/tokenHandler')
 
 const routes = {
     sample: sampleHandler,
-    user: userHandler
+    user: userHandler,
+    token: tokenHandler
 };
 
 module.exports = routes;
